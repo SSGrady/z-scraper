798ab36f394e8deaad1c2f8f4ecdbf72c2df6964
Steven Grady <stevenagrady@gmail.com>
2025-01-16 16:27:54 -0500

    Setup server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-// After installing dependencies, I've made server.js
+// Server.js manages incoming HTTPS requests and dynamically responds to them
+// i.e., keeps the server runnign and handles new searches w/t restarting server
+
+// server.js
+const express = require('express');
+
+const app = express();
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+    console.log(`Running on ${PORT}`);
+})
